891be30822d733a377c84d38564ef242121116e7
微信在线 <506232687@qq.com>
2023-02-02 12:51:49 +0800

    再来一次

--- Tool/TimeHelper.php
+++ Tool/TimeHelper.php
@@ -550,7 +550,7 @@ class TimeHelper
      * @param int|string $datetime 任意格式时间字符串或时间戳(默认为当前时间)
      * @return false|string 格式化后的时间字符串
      */
-    public static function format(string $format = 'Y-m-d H:i:s', $datetime = null): string
+    public static function format( $datetime = null,string $format = 'Y-m-d H:i:s'): string
     {
         return date($format, self::toTimestamp($datetime));
     }
@@ -583,5 +583,164 @@ class TimeHelper
     public static function daysInMonth($datetime = null): int
     {
         return intval(date('t', self::toTimestamp($datetime)));
+    }
+
+    /**
+     * 返回今日开始和结束的时间戳
+     *
+     * @return array
+     */
+    public static function today()
+    {
+        return [
+            mktime(0, 0, 0, date('m'), date('d'), date('Y')),
+            mktime(23, 59, 59, date('m'), date('d'), date('Y'))
+        ];
+    }
+
+    /**
+     * 返回昨日开始和结束的时间戳
+     *
+     * @return array
+     */
+    public static function yesterday()
+    {
+        $yesterday = date('d') - 1;
+        return [
+            mktime(0, 0, 0, date('m'), $yesterday, date('Y')),
+            mktime(23, 59, 59, date('m'), $yesterday, date('Y'))
+        ];
+    }
+
+    /**
+     * 返回本周开始和结束的时间戳
+     *
+     * @return array
+     */
+    public static function week()
+    {
+        $timestamp = time();
+        return [
+            strtotime(date('Y-m-d', strtotime("this week Monday", $timestamp))),
+            strtotime(date('Y-m-d', strtotime("this week Sunday", $timestamp))) + 24 * 3600 - 1
+        ];
+    }
+    /**
+     * 返回上周开始和结束的时间戳
+     *
+     * @return array
+     */
+    public static function lastWeek()
+    {
+        $timestamp = time();
+        return [
+            strtotime(date('Y-m-d', strtotime("last week Monday", $timestamp))),
+            strtotime(date('Y-m-d', strtotime("last week Sunday", $timestamp))) + 24 * 3600 - 1
+        ];
+    }
+    /**
+     * 返回本月开始和结束的时间戳
+     *
+     * @return array
+     */
+    public static function month()
+    {
+        return [
+            mktime(0, 0, 0, date('m'), 1, date('Y')),
+            mktime(23, 59, 59, date('m'), date('t'), date('Y'))
+        ];
+    }
+
+    /**
+     * 返回上个月开始和结束的时间戳
+     *
+     * @return array
+     */
+    public static function lastMonth()
+    {
+        $begin = mktime(0, 0, 0, date('m') - 1, 1, date('Y'));
+        $end = mktime(23, 59, 59, date('m') - 1, date('t', $begin), date('Y'));
+
+        return [$begin, $end];
+    }
+    /*
+     * 返回下个月的开始时间与结束时间
+     *
+     * @return array
+     */
+    public static function nextMonth(){
+        $begin = mktime(0, 0, 0, date('m')+1 , 1, date('Y'));
+        $end = mktime(23, 59, 59, date('m')+1, date('t', $begin), date('Y'));
+        return [$begin, $end];
+    }
+    /**
+     * 返回今年开始和结束的时间戳
+     *
+     * @return array
+     */
+    public static function year()
+    {
+        return [
+            mktime(0, 0, 0, 1, 1, date('Y')),
+            mktime(23, 59, 59, 12, 31, date('Y'))
+        ];
+    }
+
+    /**
+     * 返回去年开始和结束的时间戳
+     *
+     * @return array
+     */
+    public static function lastYear()
+    {
+        $year = date('Y') - 1;
+        return [
+            mktime(0, 0, 0, 1, 1, $year),
+            mktime(23, 59, 59, 12, 31, $year)
+        ];
+    }
+
+    /**
+     * 获取几天前零点到现在/昨日结束的时间戳
+     *
+     * @param int $day 天数
+     * @param bool $now 返回现在或者昨天结束时间戳
+     * @return array
+     */
+    public static function dayToNow($day = 1, $now = true)
+    {
+        $end = time();
+        if (!$now) {
+            list($foo, $end) = self::yesterday();
+        }
+
+        return [
+            mktime(0, 0, 0, date('m'), date('d') - $day, date('Y')),
+            $end
+        ];
+    }
+
+    /**
+     * 返回几天前的时间戳
+     *
+     * @param int $day
+     * @return int
+     */
+    public static function daysAgo($day = 1)
+    {
+        $nowTime = time();
+        return $nowTime - self::daysToSecond($day);
+    }
+
+    /**
+     * 返回几天后的时间戳
+     *
+     * @param int $day
+     * @return int
+     */
+    public static function daysAfter($day = 1)
+    {
+        $nowTime = time();
+        return $nowTime + self::daysToSecond($day);
     }
 }
